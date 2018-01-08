1da9e871b76cb9b987b4a8aa206fa40489f4619b
Stoodkev <quentin.correa@gmail.com>
2018-01-08 16:36:47 +0800

    Making the server rendering faster

--- cloud/main.js
+++ cloud/main.js
@@ -1,25 +1,63 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+var steem = require('steem');
+steem.api.setOptions({ url: 'https://api.steemit.com' });
+
+
+
+Parse.Cloud.define("checkVote", function(request, response) {
+  var aPost = Parse.Object.extend("Posts");
+  var query = new Parse.Query(aPost);
+  var post_list=[];
+  query.descending("from_length");
+  query.equalTo("voted",false);
+         query.find({
+          success: function(posts) {
+            if(posts!==undefined&&posts.length!==0)
+            {
+               for ( const [i, post] of posts.entries()){
+                  const content= steem.api.getContentAsync(post.get('url').split('@')[1].split('/')[0], post.get('url').split('/')[post.get('url').split('/').length-1]);
+                   content.then(result=> {
+                    if(result.active_votes.find(function (element) {
+                        return element.voter == 'utopian-io'||element.voter == 'utopian-1up';
+                    })!==undefined)
+                    {
+                        posts[i].set('voted',true);
+                        posts[i].save(null,{useMasterKey:true});
+                    }
+                  });
+                }
+              }
+            }
+          });
+          response.success('yea');
 });
 
-Parse.Cloud.beforeSave('Suggestions', function (request, response) {
+
+Parse.Cloud.beforeSave('Posts', function (request, response) {
     if(request.original===undefined){
-      var aSug = Parse.Object.extend("Suggestions");
-      var query = new Parse.Query(aSug);
+      var aPost = Parse.Object.extend("Posts");
+      var query = new Parse.Query(aPost);
+
       query.equalTo('url',request.object.get('url'));
       query.find( {
             useMasterKey: true,
-            success: function (sug) {
-              console.log(sug);
-              if(sug.length===0)
+            success: function (post) {
+              console.log(post);
+              if(post.length===0)
               {
-                response.success();
+                const content= steem.api.getContentAsync(request.object.get('url').split('@')[1].split('/')[0], request.object.get('url').split('/')[request.object.get('url').split('/').length-1]);
+                 content.then(result=> {
+                   request.object.set('title', result.title);
+                   request.object.set('author', result.author);
+                   request.object.set('reputation',steem.formatter.reputation(result.author_reputation));
+
+                   response.success();
+                     });
               }
               else
               {
-                console.log(sug[0].get('from'));
-                var from=sug[0].get('from');
+                console.log(post[0].get('from'));
+                var from=post[0].get('from');
                 if(from.includes(request.object.get('from')[0])){
                   response.error('You can only vote once! ');
                   return
@@ -28,19 +66,22 @@ Parse.Cloud.beforeSave('Suggestions', function (request, response) {
 
                   from.push(request.object.get('from')[0]);
                     console.log(from);
-                    sug[0].set('from',from);
-                    sug[0].set('from_length',from.length);
-                    sug[0].set('voted', false);
-                    sug[0].set('url',sug[0].get('url'));
-                  request.object=sug[0];
-                  sug[0].destroy({useMasterKey:true});
+                    post[0].set('from',from);
+                    post[0].set('from_length',from.length);
+                    post[0].set('voted', false);
+                    post[0].set('url',post[0].get('url'));
+                    post[0].set('title',post[0].get('title'));
+                    post[0].set('author',post[0].get('author'));
+                    post[0].set('reputation',post[0].get('reputation'));
+                  request.object=post[0];
+                  post[0].destroy({useMasterKey:true});
                   response.success();
-
                 }
               }
             }
             ,error:function(err){console.log(err);}
           });
-    }
-    else response.success();
+
+}
+  else response.success();
 });
