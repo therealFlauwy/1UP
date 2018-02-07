6e923f1227bc1c067a21f83a8c23280e7215e0bc
Stoodkev <quentin.correa@gmail.com>
2018-02-07 14:27:50 +0800

    Switch to trail

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,6 @@
 var steem = require('steem');
 const MAX_VOTE_PER_DAY=10;
-const BOT='steem-plus';
+const BOT='utopian-1up';
 steem.api.setOptions({ url: 'https://api.steemit.com' });
 let sc2=require('sc2-sdk');
 
