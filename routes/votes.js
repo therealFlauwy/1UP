8d143bb148a17a36df63fadf72f1d7aaecc944b6
Stoodkev <quentin.correa@gmail.com>
2018-10-08 17:53:17 +0800

    Handle callbacks from the cloud

--- routes/votes.js
+++ routes/votes.js
@@ -8,7 +8,15 @@ module.exports = function(app,steem,Utils,config,messages){
         vote.set("url",req.body.url);
         vote.set("community",req.body.community);
         // Create the vote object sent to be processed in the cloud
-        vote.save();
+        vote.save().then((result) => {
+          // Execute any logic that should take place after the object is saved.
+          res.sendStatus(200);
+        }, (error) => {
+          console.log(error.message);
+          // Execute any logic that should take place if the save fails.
+          // error is a Parse.Error with an error code and message.
+          res.status(500).send({error:error.message});
       });
   });
+});
 }
