3828d90b34a05c2a1774a55c3e5cc47702c618b3
Stoodkev <quentin.correa@gmail.com>
2018-10-08 17:33:47 +0800

    Create Vote object and send it to be processed in the cloud

--- routes/votes.js
+++ routes/votes.js
@@ -2,8 +2,13 @@ module.exports = function(app,steem,Utils,config,messages){
   // Posts page community
   app.post("/vote", function(req, res) {
       Utils.getSession(req).then(function(session) {
-        console.log(req.params,session);
-        res.sendStatus(200);
+        const Votes = Parse.Object.extend("Votes");
+        let vote= new Votes();
+        vote.set("voter",session.name);
+        vote.set("url",req.body.url);
+        vote.set("community",req.body.community);
+        // Create the vote object sent to be processed in the cloud
+        vote.save();
       });
   });
 }
